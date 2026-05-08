72f375acc448734a9913cb14b7da7fa849488156
Atharva Awasthi <atharva123aa@gmail.com>
2026-05-08 16:13:30 +0530

    Update Flask app to handle student data and connections to html files
    Refactor Flask app to manage student names with file operations.

--- app.py
+++ app.py
@@ -1,3 +1,38 @@
-from flask import, render_tempelate flask
-app= Flask (__name__)
-@app.route 
+from flask import Flask, render_template, request,redirect
+import os as m
+app = Flask(__name__)
+
+STUDENTS_file="students.txt"
+def get_students():
+    def   save student (name):
+         with open   (STUDENTS_file,"a") as f:
+            f.write(name + "\n")
+        with open
+    if not   (m.path.exists(STUDENTS_file):
+        return []
+
+        with open  (STUDENTS_file,"r") as f:
+            names=[line.script() for line in f.readlines()]
+            [return n for n in names if n !=""]
+
+
+@app.route("/") 
+def home():
+    return render_template("index.html")
+  @app.route("/") 
+def students():
+    all_students=get_students()
+return render_template("students.html", students=all_students)
+
+
+@app.route("/add student", methods=["POST"])
+def add_student():
+    name=request.form.get("name")
+     if name and name.strip() !="":
+        
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)      
